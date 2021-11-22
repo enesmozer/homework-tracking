8eb439cf5a75428bee9c80a4e5e10a72f5715143
Enes Malik Özer <enesmozer@gmail.com>
2021-11-22 22:22:47 +0300

    utils for api call

---
+++ src/utils/http.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+const http = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+    "X-Application-Name": "market",
+  },
+  baseURL: process.env.REACT_APP_API_URL,
+});
+http.interceptors.response.use(
+  (response) => {
+    if (response.data) {
+      if (response.data.statusCode === 200 && response.data.message) {
+        return response.data;
+      }
+    }
+    return response;
+  },
+  (error) => {
+    const { response } = error;
+    const { statusCode } = response;
+    if (statusCode === 401) {
+      window.location.href = window.location.pathname;
+      return new Promise(() => {});
+    }
+    if (statusCode === 403) {
+      return new Promise(() => {});
+    }
+    if (response.message) {
+      return response.message;
+    }
+    return Promise.reject(error);
+  }
+);
+export default http;
