7059360b5db018d3a45a335674a32763f55b5cc1
Enes Malik Özer <enesmozer@gmail.com>
2021-11-23 15:03:12 +0300

    routes added

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
 import store from "./redux/configureStore";
-import { Provider } from 'react-redux';
-
+import { Provider } from "react-redux";
+import TeacherList from "./components/TeacherList";
+import LoginForm from "./components/LoginForm";
+import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="teachers" element={<TeacherList />} />
+        <Route path="login" element={<LoginForm />} />
+      </Routes>
+      <App />
+    </BrowserRouter>
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
