c81bd146157f9a151004ca3687f5363e83bdbed4
Enes Malik Özer <enesmozer@gmail.com>
2021-11-23 15:02:33 +0300

    added login user

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const navigate = useNavigate();
+  const loginUser = localStorage.getItem("loginUser");
+  useEffect(() => {
+    if (!loginUser) navigate("/login");
+  }, [loginUser,navigate]);
 
+  return <div className="main"></div>;
+}
 export default App;
