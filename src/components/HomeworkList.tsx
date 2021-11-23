46c32891e0058ec4becbbbf8cc58362ece576723
Enes Malik Özer <enesmozer@gmail.com>
2021-11-23 21:27:12 +0300

    upload homework added

--- src/components/HomeworkList.tsx
+++ src/components/HomeworkList.tsx
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { getHomeworksByStudent } from "../redux/actions";
-import { Spin, Switch, Table } from "antd";
+import { Spin, Switch, Table, Upload, message, Button } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
 function HomeworkList() {
   const dispatch = useDispatch();
@@ -34,16 +35,49 @@ function HomeworkList() {
         <Switch checked={isDone} onChange={completeHomeWork} />
       ),
     },
+    {
+      title: "Upload",
+      dataIndex: "",
+      key: "",
+      render: (text: string, record: any) => (
+        <Upload
+          {...props}
+          onChange={(info: any) => {
+            if (info.file.status !== "uploading") {
+              console.log(info.file, info.fileList);
+            }
+            if (info.file.status === "done") {
+              message.success(`${info.file.name} file uploaded successfully`);
+              record.isDone = true;
+            } else if (info.file.status === "error") {
+              message.error(`${info.file.name} file upload failed.`);
+            }
+          }}
+        >
+          <Button icon={<UploadOutlined />}>Click to Upload</Button>
+        </Upload>
+      ),
+    },
   ];
   const navigate = useNavigate();
   const loginUser = JSON.parse(localStorage.getItem("loginUser") || "{}");
   useEffect(() => {
-    if (loginUser.role !== "principle" && loginUser.studentId !== params.studentId) {
+    if (
+      loginUser.role !== "principle" &&
+      loginUser.studentId !== params.studentId
+    ) {
       localStorage.removeItem("loginUser");
       navigate("/login");
     }
-  }, [navigate]);
+  }, [navigate, loginUser, params.studentId]);
 
+  const props = {
+    name: "file",
+    action: "https://jsonplaceholder.typicode.com/posts/",
+    headers: {
+      authorization: "authorization-text",
+    },
+  };
   return (
     <div className="homework">
       {homeworks.length > 0 ? (
